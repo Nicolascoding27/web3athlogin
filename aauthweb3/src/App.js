8a77293222029dc0574b01612cccaaf3ba2f9bd6
Nicolas Felipe Diaz <68035598+Nicolascoding27@users.noreply.github.com>
2022-08-01 21:48:54 -0500

    DID creation working, deploying on the way

--- aauthweb3/src/App.js
+++ aauthweb3/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import {useState} from 'react';
 import React from 'react';
 import { CeramicClient } from '@ceramicnetwork/http-client';
-import {ThreeIdResolver } from '@ceramicnetwork/3id-did-resolver'
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
 import { EthereumAuthProvider, ThreeIdConnect} from '@3id/connect';
 import { IDX } from '@ceramicstudio/idx';
+import { DID } from 'dids';
 //defining the endpoint node that we will be using 
 const endpoint = "https://ceramic-clay.3boxlabs.com"
 
@@ -43,8 +44,8 @@ function App() {
       if(data.name){
         setName(data.name)
       } 
-      if(data.image){
-        setImage(data.image)
+      if(data.avatar){
+        setImage(data.avatar)
       } 
     } catch (error) { //A potential error could happen if there is not an user logged in
       console.log('error:', error)
@@ -52,10 +53,44 @@ function App() {
       setLoaded(true); //the site loaded and this will be after we read the user's profile
     }
   }
+  async function updateProfile(){
+    const [address]=await connect();
+    const ceramic= new CeramicClient(endpoint);
+    //Using threIdconnect, creating an instance 
+    const threeIdConnect= new ThreeIdConnect();
+    console.log('instance', threeIdConnect)
+    const provider = new EthereumAuthProvider(window.ethereum, address);
+    //Conecting the instance of 3id connect to the provider 
+    await threeIdConnect.connect(provider);
+    //creating the did based on the provider (WALLET ) and the resolver
+    //This will create or get a reference based on the user id 
+    const did = new DID({
+      provider: threeIdConnect.getDidProvider(),
+      resolver: {
+        ...ThreeIdResolver.getResolver(ceramic)
+      }
+    })
+    //Setting the DID
+    ceramic.setDID(did)
+    //Authenticating the user 
+    await ceramic.did.authenticate() //We are authenticated with the user waller
+    const idx= new IDX({ceramic});
+    await idx.set(
+      'basicProfile', {
+        name,
+        avatar:image
+      }
+    )
+    alert('Profile updated :) ')
+  }
   return (
     <div className="App">
+  <input placeholder="Name" onChange={e => setName(e.target.value)} />
+  <input placeholder="Profile Image" onChange={e => setImage(e.target.value)} />
+  <button onClick={updateProfile}>Set Profile</button>
     <button onClick={readProfile}>Log in / Set profile</button>
-
+    { name && <h3>{name}</h3> }
+      { image && <img style={{ width: '400px' }} src={image} /> }
     </div>
   );
 }
